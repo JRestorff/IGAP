36f2bc4f777ce5348530e9a1f750588dcfa9d339
JRestorff <restorff@t-online.de>
2020-08-09 11:02:10 +0200

    Änderung vom 9.8.2020

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
 
+// Navigation, Menu
 
-/* Navigation Main Menü Desktop*/
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
 
-var nav = document.getElementById('nav');
-var navItems = document.querySelectorAll('.menu__item');
-var navLine = document.querySelector('.nav-line');
-var navActive = document.querySelector('#menu .active');
-var navPadding = parseFloat(window.getComputedStyle(nav, null).getPropertyValue('padding-top'));
+navToggle.addEventListener('click', () => {    
+    links.classList.toggle('show-links');
+});
 
-// Loop through nav items
-for (var i = 0; i < navItems.length; i++) {
-    // Animate nav line on hover
-    navItems[i].addEventListener("mouseenter", function(e) {
-        navLine.style.marginTop = this.offsetTop + (this.offsetHeight / 2) - 15 + navPadding + 'px';
-    });
-    // Reset nav line
-    navItems[i].addEventListener("mouseleave", function(e) {
-        navLine.style.marginTop =  navActive.offsetTop + (navActive.offsetHeight / 2) - 15 + navPadding + 'px';
-    });
-}
-
-// Set nav line position for active menu item
-window.onload = function() {
-    navLine.style.marginTop = navActive.offsetTop + (navActive.offsetHeight / 2) - 15 + navPadding + 'px';
-}
